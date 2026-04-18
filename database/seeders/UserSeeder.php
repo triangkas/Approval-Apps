532f7dcfa0fbdc12fd0199882fcb3888404c2024
Tri Anggoro Kasih <triangkas@gmail.com>
2026-04-18 14:33:18 +0700

    [new] engine

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'id' => '2',
+            'name' => 'Administrator',
+            'email' => 'admin@domain.id',
+            'username' => 'admin',
+            'password' => bcrypt('adminpass'),
+        ]);
+    }
+}
